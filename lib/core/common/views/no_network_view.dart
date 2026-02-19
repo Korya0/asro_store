249f5b4eabf51643404ec60fd059e775dd52c823
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-02-20 01:39:01 +0200

    appp icon and name

---
+++ lib/core/common/views/no_network_view.dart
@@ -0,0 +1,21 @@
+import 'package:asroo_store/core/style/images/app_images.dart';
+import 'package:flutter/material.dart';
+
+class NoNetworkView extends StatelessWidget {
+  const NoNetworkView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppImages.noNetwork),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:asroo_store/core/app/connectivity_controller.dart';
 import 'package:asroo_store/core/app/env_varible.dart';
+import 'package:asroo_store/core/common/views/no_network_view.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -6,6 +10,7 @@ import 'package:flutter/services.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EnvVariable.instance.init(envType: EnvType.dev);
+
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
     DeviceOrientation.portraitUp,
@@ -21,11 +26,42 @@ class AsroStoreApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-      home: const Scaffold(
-        backgroundColor: Colors.black,
-      ),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+
+      builder: (_, value, _) {
+        if (value) {
+          return MaterialApp(
+            title: 'Asroo Store',
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            builder: (context, child) {
+              return Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    unawaited(ConnectivityController.instance.init());
+                    return child!;
+                  },
+                ),
+              );
+            },
+            home: const Scaffold(
+              body: Center(
+                child: Text('Asroo Store'),
+              ),
+            ),
+          );
+        } else {
+          return MaterialApp(
+            title: 'No Network',
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            home: const NoNetworkView(),
+          );
+        }
+      },
     );
   }
 }
